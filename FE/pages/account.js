d8ecb5b885a5a75e41a2b96f7282f87028111d0a
Serban <serban.lupea@evozon.com>
2020-03-27 19:19:50 +0200

    account page

--- FE/pages/account.js
+++ FE/pages/account.js
@@ -1,14 +1,34 @@
 import Layout from '../components/Layout';
-import Login from '../components/Login'
+//mport Login from '../components/Login'
+import GoogleLogin from 'react-google-login'
+import React, { useState } from 'react'
+import ProfileDetails from '../components/ProfileDetails'
+import { isEmpty } from 'lodash'
 
 
-const Account = ({props}) => {
+const Account = () => {
+  const [profileObj, setProfileObj] = useState({})
+
+  const responseGoogle = (response) => {
+    setProfileObj(response.profileObj)
+  }
+
+  const failGoogle = (response) => {
+    console.log('Login failed!', response)
+  }
   return (
     <Layout>
       <p>Account</p>
-      <Login>
-        
-      </Login>
+      <GoogleLogin
+        clientId="591615586801-jt473emak2kluka567ch21om85n8ps0b.apps.googleusercontent.com"
+        buttonText="Login"
+        clientSecret="ZjW-HBvyBcVWbBq4VP72DG8B"
+        onSuccess={responseGoogle}
+        onFailure={failGoogle}
+      >
+      </GoogleLogin>
+      {!isEmpty(profileObj) ? <ProfileDetails props={profileObj} /> : <p></p>}
+
     </Layout>
   );
 }
